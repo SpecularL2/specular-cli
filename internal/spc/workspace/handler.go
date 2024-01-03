04f67072e39f1d3429fd6c7ee7f6d3ea13637b00
siosw <git@siosw.net>
2024-01-03 15:52:26 +0100

    github api used to download config

--- internal/spc/workspace/handler.go
+++ internal/spc/workspace/handler.go
@@ -1,20 +1,30 @@
 package workspace
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/user"
 
-	"github.com/hashicorp/go-getter"
-
 	"github.com/SpecularL2/specular-cli/internal/service/config"
+	"github.com/sirupsen/logrus"
 )
 
-const defaultWorkspaceUrl = "github.com/SpecularL2/specular//config/local_devnet"
+const defaultRepository = "specularL2/specular"
+const defaultWorkspacePath = "config/local_devnet"
+const githubUrl = "https://api.github.com/repos/%s/contents/%s"
 
 type WorkspaceHandler struct {
 	cfg *config.Config
+	log *logrus.Logger
+}
+
+type ConfigFile struct {
+	DownloadUrl string `json:"download_url"`
+	Name string `json:"name"`
 }
 
 func (w *WorkspaceHandler) DownloadDefault() error {
@@ -23,46 +33,79 @@ func (w *WorkspaceHandler) DownloadDefault() error {
 		return err
 	}
 
-	var dst = fmt.Sprintf("%s/.spc/workspaces/default", usr.HomeDir)
+	dst := fmt.Sprintf("%s/.spc/workspaces/default", usr.HomeDir)
+	// TODO: ask for confirmation if workspace already exists 
+	if err = os.RemoveAll(dst); err != nil {
+		return err
+	}
+	if err = os.MkdirAll(dst, os.ModePerm); err != nil {
+		return err
 
-	client := &getter.Client{
-		Ctx: context.Background(),
-		// Define the destination to where the directory will be stored.
-		// This will create the directory if it doesn't exist.
-		Dst: dst,
-		Dir: true,
-		// The repository with a subdirectory I would like to clone only.
-		Src:  defaultWorkspaceUrl,
-		Mode: getter.ClientModeDir,
-		// Define the type of detectors go getter should use, in this case only GitHub is needed
-		Detectors: []getter.Detector{
-			&getter.GitHubDetector{},
-		},
-		// Provide the getter needed to download the files
-		Getters: map[string]getter.Getter{
-			"git": &getter.GitGetter{},
-		},
 	}
-	// Download the files
-	if err := client.Get(); err != nil {
-		fmt.Printf("Error getting path %s: %v", client.Src, err)
-		os.Exit(1)
+	w.log.Infof("saving workspace at: %s", dst)
+
+	orig := fmt.Sprintf(githubUrl, defaultRepository, defaultWorkspacePath)
+	resp, err := http.Get(orig)
+	if err != nil {
+		return err
 	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	files := []ConfigFile{}
+	err = json.Unmarshal(body, &files)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		filePath := dst + "/" + file.Name
+		if err = w.downloadFile(filePath, file.DownloadUrl); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
 func (w *WorkspaceHandler) Cmd() error {
 	if w.cfg.WorkspaceCmd.Command == "download" && w.cfg.WorkspaceCmd.Name == "default" {
-		fmt.Printf("Downloading default workspace from: %s\n", defaultWorkspaceUrl)
 		if err := w.DownloadDefault(); err != nil {
 			return err
 		}
+	} else {
+		return fmt.Errorf("the only supported command is:\nspc workspace download default")
 	}
 	return nil
 }
 
-func NewWorkspaceHandler(cfg *config.Config) *WorkspaceHandler {
+func (w *WorkspaceHandler) downloadFile(filepath string, url string) error {
+	w.log.Tracef("donwloading file: %s\n", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	w.log.Tracef("saving at: %s\n", filepath)
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func NewWorkspaceHandler(cfg *config.Config, log *logrus.Logger) *WorkspaceHandler {
 	return &WorkspaceHandler{
 		cfg: cfg,
+		log: log,
 	}
 }
